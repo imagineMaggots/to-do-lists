86e3575bd1eb7584008372557156ce0a61e4c684
Hendrik <hendriktheede@gmail.com>
2025-11-26 09:12:54 +0100

    Create main.cxx

---
+++ main.cxx
@@ -0,0 +1,16 @@
+/**
+ * a simple to-do list program
+ */
+#include <iostream>
+#include <fstream>
+int main(int argc, char** argv)
+{
+    std::cout << "Hello" << std::endl;
+    // Load current To-Do's
+    // Edit To-Do's
+    // Add notes to To-Do's
+    // Save To-Do's
+    // Finish/Restore some old To-Do's
+    // 
+    return NULL;
+}
